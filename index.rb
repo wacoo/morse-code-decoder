9618b98c14b1e3bc8033462318778d918c7a4d4f
wacoo <wababam9@gmail.com>
2023-10-04 22:47:07 -0700

    Fix linter error

--- index.rb
+++ index.rb
@@ -1,29 +1,34 @@
-LIST_OF_CHARS = {'.-'=> 'A', '-...'=> 'B', '-.-.'=> 'C', '-..'=> 'D', '.'=> 'E', '..-.'=> 'F', '--.'=> 'G', '....'=> 'H', '..'=> 'I', '.---'=> 'J', '-.-'=> 'K', '.-..'=> 'L', '--'=> 'M', '-.'=> 'N', '---'=> 'O', '.--.'=> 'P', '--.-'=> 'Q', '.-.'=> 'R', '...'=> 'S', '-'=> 'T', '..-'=> 'U', '...-'=> 'V', '.--'=> 'W', '-..-'=> 'X', '-.--'=> 'Y', '--..'=> 'Z', '.----'=> 1, '..---'=> 2, '...--'=> 3, '....-'=> 4, '.....'=> 5, '-....'=> 6 , '--...'=> 7, '---..'=> 8, '----.'=> 9, '-----'=> 0};
+LIST_OF_CHARS = { '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G',
+                  '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N',
+                  '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U',
+                  '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z', '.----' => 1, '..---' => 2,
+                  '...--' => 3, '....-' => 4, '.....' => 5, '-....' => 6, '--...' => 7, '---..' => 8, '----.' => 9,
+                  '-----' => 0 };
 def decode_char(char)
-    # decodes a morse code character
-    if LIST_OF_CHARS.key?(char)
-        return LIST_OF_CHARS[char]
-    end
+  # decodes a morse code character
+  if LIST_OF_CHARS.key?(char)
+    return LIST_OF_CHARS[char]
+  end
 end
 
 def decode_word(str)
-    # decode a morse code word
-    new_word = [];
-    chars = str.split
-    chars.each do |code|
-        new_word.push(decode_char(code))
-    end
-    return new_word.join('')
+  # decode a morse code word
+  new_word = [];
+  chars = str.split
+  chars.each do |code|
+    new_word.push(decode_char(code))
+  end
+  return new_word.join('')
 end
 
 def decode(str)
-    # decode morse code
-    new_string = []
-    words = str.split('   ')
-    words.each do |word|
-          new_string.push(decode_word(word))
-    end
-    return new_string.join(' ')
+  # decode morse code
+  new_string = []
+  words = str.split('   ')
+  words.each do |word|
+    new_string.push(decode_word(word))
+  end
+  return new_string.join(' ')
 end
 
 puts decode_char('.-')
